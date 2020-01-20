02612a877496a4d933b49aad0b790dd13fddb68f
Vladislav A. Sidorin <v.sidorin@rabota.ru>
2020-01-20 18:56:19 +0300

    тестовые задания, не все

---
+++ test1.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Нам нужно скопирвать данные из массива в SplFixedArray с обязательным приведением их к некоторому типу
+ * Что не так с этим кодом? Почему он такой медленный?
+ */
+
+define("NUM", 1000000);
+
+function typeCast($in, string $type)
+{
+    switch ($type) {
+        case 'int': return (int)$in;
+        case 'float':
+        case 'double': return (float)$in;
+        default: return (string)$in;
+    }
+}
+
+$arr = [];
+for($i = 0; $i < NUM; $i++) {
+    $arr[$i] = mt_rand(1, 100);
+}
+
+$matrix = new SplFixedArray(NUM);
+$i = 0;
+array_map(function ($value) use ($matrix, &$i) {
+    $matrix[$i] = typeCast($value, 'float');
+    $i++;
+}, $arr);
+
+?>
