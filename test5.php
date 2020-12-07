f6081f85bce3a328b6eeb516c356e9baf8474bc7
Vladislav A. Sidorin <v.sidorin@rabota.ru>
2020-12-07 17:56:13 +0300

    задание 5 - переделано

--- test5.php
+++ test5.php
@@ -1,15 +1,54 @@
 <?php
-//Дано множество
-/** @var int[] $set */
-$set = [1,1,2,5,7,3,5,3,9];
 
-//напишите функцию, которая выведет все подмножества этого множества, включая пустое множество
+class TreeNode {
+    /** @var int */
+    public $value;
+    /** @var \TreeNode */
+    public $left;
+    /** @var \TreeNode */
+    public $right;
+
+    public function __construct(
+      int $value,
+      ?TreeNode $left = null,
+      ?TreeNode $right = null
+    ) {
+        $this->value = $value;
+        $this->left = $left;
+        $this->right = $right;
+    }
+}
 
 /**
- * @param int[] $set
+ * Реализуйте функцию, выводящую все числа в дереве в любой последовательности
  *
- * @return int[][]
+ * @param \TreeNode|null $node
+ *
+ * @return string
  */
-function SubSet(array $set): array {
-
+function renderTree(?TreeNode $node): string
+{
+    return "";
 }
+
+echo renderTree(generateTree());
+
+function generateTree(): TreeNode
+{
+    $root = new TreeNode(11);
+    $root->left = new TreeNode(5);
+    $root->left->left = new TreeNode(3);
+    $root->left->left->left = new TreeNode(2);
+    $root->left->right = new TreeNode(7);
+    $root->right = new TreeNode(41);
+    $root->right->left = new TreeNode(23);
+    $root->right->left->right = new TreeNode(29);
+    $root->right->left->left = new TreeNode(19);
+    $root->right->right = new TreeNode(53);
+    $root->right->right->left = new TreeNode(47);
+    $root->right->right->right = new TreeNode(101);
+    $root->right->right->right->right = new TreeNode(107);
+    $root->right->right->right->left = new TreeNode(89);
+    return $root;
+}
+
