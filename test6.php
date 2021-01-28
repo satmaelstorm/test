5509f897700258c1a1b6e1a442748732a842ce3b
Vladislav A. Sidorin <v.sidorin@rabota.ru>
2021-01-28 10:47:19 +0300

    test6

---
+++ test6.php
@@ -0,0 +1,39 @@
+<?php
+declare(strict_types=1);
+
+define("INDEX_LEN", 10000);
+define("SEARCH_SEQUENCE_LENGTH", 100000);
+define("MAX_RAND", 1000000);
+
+class Index {
+    private $index = [];
+
+    public function __construct(int $len)
+    {
+        for ($i = 0; $i < $len; $i++) {
+            $this->index[$i] = mt_rand(0, MAX_RAND);
+        }
+    }
+
+    //ваш код тут. Помимо реализации функции поиска допустима предобработка индекса (можно и в конструкторе)
+
+    public function search(int $val): bool
+    {
+        return false;
+    }
+
+}
+$index = new Index(INDEX_LEN);
+$results = [];
+$timeStart = microtime(true);
+
+for ($i = 0; $i < SEARCH_SEQUENCE_LENGTH; $i++) {
+    $r = mt_rand(0, MAX_RAND);
+    if ($index->search($r)) {
+        isset($results[$i]) ? $results[$i]++ : $results[$i] = 1;
+    }
+}
+
+$timeElapsed = microtime(true) - $timeStart;
+echo sprintf("time elapsed %f sec\n", $timeElapsed);
+echo json_encode($results, JSON_PRETTY_PRINT) . "\n";
