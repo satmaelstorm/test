71c8917897ed96db1de7095132160cc2c0e9a828
Vladislav Sidorin <v.sidorin@rabota.ru>
2023-09-21 15:10:18 +0300

    add task

--- test7.php
+++ test7.php
@@ -3,10 +3,10 @@ declare(strict_types=1);
 
 class MoneyTransfer {
 
-	public function transfer($from, $to, $amount): void
+	public function transfer($from, $to, $amount, $currency): void
 	{
-		DB::query("update account set amount=amount - $amount where user_id = {$from->getId()}");
-		DB::query("update account set amount=amount + $amount where user_id = {$to->getId()}");
+		DB::query("update account set amount=amount - $amount where user_id = {$from->getId()} and currency='$currency'");
+		DB::query("update account set amount=amount + $amount where user_id = {$to->getId()} and currency='$currency'");
 	}
 
 }
